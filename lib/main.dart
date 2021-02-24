93f967626c7e1aceaf3c0baf76a206bdfb92881f
Lukita da Galera <lucasgo1099@gmail.com>
2021-02-23 23:23:34 -0300

    finish appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
+const request = "";
 
-  void main() {
-    runApp(MaterialApp(
-      home: Home(),
-    ));
-  }
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+          hintStyle: TextStyle(color: Colors.amber),
+        )),
+  ));
+}
 
-  class Home extends StatefulWidget {
-    @override
-    _HomeState createState() => _HomeState();
-  }
+// Vai retornar os dados da API no futuro
+Future<Map> getApi() async {
+  // Fazendo requisição assinrona
+  http.Response response = await http.get(request);
+  // Para chamar o JSON devemos importar a biblioteca convert
+  return json.decode(response.body);
+}
 
-  class _HomeState extends State<Home> {
-    @override
-    Widget build(BuildContext context) {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
 
-      return Scaffold(
-
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       backgroundColor: Colors.blueGrey[50],
-          appBar: AppBar(
-          title: Text("Converta seus Bitcoins ", style: TextStyle(fontFamily: 'Raleway-Regular.tff', color: Colors.grey[900], fontSize: 25.0), ),
-          backgroundColor: Colors.blueGrey[50],
-          centerTitle: true,
-          elevation: 0,
+      appBar: AppBar(
+        title: Text(
+          "Converta seus Bitcoins ",
+          style: TextStyle(
+              fontFamily: 'Raleway-Regular.tff',
+              color: Colors.grey[900],
+              fontSize: 25.0),
         ),
+        backgroundColor: Colors.blueGrey[50],
+        centerTitle: true,
+        elevation: 0,
+      ),
 
-        body: Center(
-          child: Text("Vamos converter Bitcoins em Reais"),
-             
-          ),
-      );
-    }
+    body: FutureBuilder<Map> (
+      future: getApi(),  
+      builder: (context, snapshot){
+          // Swith case para conexão da API com Aplicativo
+          switch(snapshot.connectionState){
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+               // Retornando os dados no meio da tela
+               
+
+          }
+      },
+      
+    ) 
+    
+    
+    );
   }
+}
